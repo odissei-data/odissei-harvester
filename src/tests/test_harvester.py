f0e432ededd84d12b20753ac92f7ecbc22e0964b
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2023-10-23 11:55:17 +0200

    Test are working now.

--- src/tests/test_harvester.py
+++ src/tests/test_harvester.py
@@ -1,102 +1,82 @@
+import os
 from datetime import datetime
+from unittest.mock import patch
 
 import moto as moto
-import pytest
-from unittest.mock import Mock, patch
-
 from starlette.testclient import TestClient
-
-from harvester import extract_records, extract_identifiers, harvest_oai_pmh
-from models import Harvest
+from models import Harvest, HarvestStatus
 from server import create_server
+
+api_key = os.environ.get("API_KEY")
 
 
 class DummyHarvestRepo:
     def create_harvest(self):
-        return ''
+        return Harvest(id=1, harvest_id=1, start_time=datetime.now(),
+                       status=HarvestStatus.PENDING)
 
-    def update_harvest_failed(self):
-        pass
+    def update_harvest_failed(self, harvest_id, error_message):
+        return Harvest(id=1, harvest_id=harvest_id, start_time=datetime.now(),
+                       status=HarvestStatus.FAILED,
+                       error_message=error_message)
 
-    def update_harvest_success(self):
-        pass
+    def update_harvest_success(self, harvest_id):
+        return Harvest(id=1, harvest_id=harvest_id, start_time=datetime.now(),
+                       status=HarvestStatus.COMPLETED)
 
-    def get_harvest_status(self):
-        return Harvest(harvest_id=1, start_time=datetime.now())
+    def get_harvest_status(self, harvest_id):
+        return Harvest(id=1, harvest_id=harvest_id, start_time=datetime.now(),
+                       status=HarvestStatus.COMPLETED)
 
 
+headers = {"X-API-Key": api_key}
 s3client = moto.mock_s3()
 repo = DummyHarvestRepo()
 server = create_server(s3client, repository=repo)
 test_client = TestClient(app=server)
 
 
-def test_version_endpoint(test_client):
-    response = test_client.get("/version")
-    assert response.status_code == 200
-    assert response.json() == {"version": "your_version_here"}
-
-
-def test_start_liss_harvest_background_endpoint(test_client):
-    # Create a sample LISSRequest
-    liss_request_data = {
-        "param1": "value1",
-        "param2": "value2",
-        # Add other required parameters
-    }
-
-    response = test_client.post("/start_liss_harvest_background",
-                                json=liss_request_data)
-    assert response.status_code == 200
-
-    # Assuming your API returns the created harvest status
-    assert "harvest_id" in response.json()
-
-
-def test_start_liss_harvest_endpoint():
-    # Create a sample LISSRequest
-    liss_request_data = {
-        "param1": "value1",
-        "param2": "value2",
-        # Add other required parameters
-    }
-
-    response = test_client.post("/start_liss_harvest", json=liss_request_data)
-    assert response.status_code == 200
-
-    # Assuming your API returns the created harvest status
-    assert "harvest_id" in response.json()
+def test_start_liss_harvest_background_endpoint():
+    with patch('S3Client.create_bucket_if_not_exists', return_value=None):
+        liss_request_data = {
+            "bucket_name": "liss-harvest-test"
+        }
+        response = test_client.post("/start_liss_harvest_background",
+                                    json=liss_request_data, headers=headers)
+        assert response.status_code == 200
+        assert "harvest_id" in response.json()
 
 
 def test_start_harvest_background_endpoint():
-    # Create a sample HarvestRequest
-    harvest_request_data = {
-        "param1": "value1",
-        "param2": "value2",
-        # Add other required parameters
-    }
-
-    response = test_client.post("/start_harvest_background",
-                                json=harvest_request_data)
-    assert response.status_code == 200
-
-    # Assuming your API returns the created harvest status
-    assert "harvest_id" in response.json()
+    with patch('S3Client.create_bucket_if_not_exists', return_value=None):
+        harvest_request_data = {
+            "metadata_prefix": "oai_dc",
+            "oai_set": "Groningen_Social_Sciences",
+            "oai_endpoint": "https://dataverse.nl/oai",
+            "bucket_name": "groningen-identifier-test",
+            "verb": "ListIdentifiers"
+        }
+        response = test_client.post("/start_harvest_background",
+                                    json=harvest_request_data,
+                                    headers=headers)
+        assert response.status_code == 200
+        assert "harvest_id" in response.json()
 
 
 def test_start_harvest_endpoint():
-    # Create a sample HarvestRequest
-    harvest_request_data = {
-        "param1": "value1",
-        "param2": "value2",
-        # Add other required parameters
-    }
-
-    response = test_client.post("/start_harvest", json=harvest_request_data)
-    assert response.status_code == 200
-
-    # Assuming your API returns the created harvest status
-    assert "harvest_id" in response.json()
+    with patch('S3Client.create_bucket_if_not_exists', return_value=None):
+        harvest_request_data = {
+            "metadata_prefix": "oai_dc",
+            "oai_set": "Groningen_Social_Sciences",
+            "oai_endpoint": "https://dataverse.nl/oai",
+            "bucket_name": "groningen-identifier-test",
+            "verb": "ListIdentifiers"
+        }
+        response = test_client.post("/start_harvest",
+                                    json=harvest_request_data,
+                                    headers=headers)
+        assert response.status_code == 200
+        assert "harvest_id" in response.json()
 
 
 def test_get_harvest_status_endpoint():
@@ -108,12 +88,3 @@ def test_get_harvest_status_endpoint():
 
     # Assuming your API returns the harvest status for the given harvest_id
     assert "harvest_id" in response.json()
-
-
-def test_get_harvest_status_endpoint_invalid_id():
-    # Create an invalid harvest_id for testing (replace with an invalid harvest_id)
-    invalid_harvest_id = "invalid_id"
-
-    response = test_client.get(f"/harvest_status/{invalid_harvest_id}")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Harvest not found"}
