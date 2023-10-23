35868d463ee72cdfcb76d6a21c37c5e1ec8e336b
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MBP.ia.knaw.local>
2023-10-23 11:52:09 +0200

    Changed module according to newer version sqlalchemy.

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 POSTGRES_DB_URL = os.environ['POSTGRES_DB_URL']
